567f78163dce7b0c1a3748b485f3e68704fdfb0f
Joseph Kuriackal <jokuriackal@gmail.com>
2015-08-21 07:37:25 +0530

    updated the resp_serv

--- src/servMon/resp_serv.java
+++ src/servMon/resp_serv.java
@@ -11,11 +11,16 @@ package servMon;
 
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+
 import java.sql.*;
 
 /**
@@ -34,7 +39,8 @@ public class resp_serv extends HttpServlet {
 	
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("hello");
+System.out.println("hello");
+		
 		Connection conn=null;
 		try
 		{
@@ -43,45 +49,35 @@ public class resp_serv extends HttpServlet {
 			conn=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","scott","tiger");
 			
 			Statement s=conn.createStatement();
-			disp();
 			
-			ResultSet rs1=s.executeQuery("update kar set age=99 where name='kar'");
+			System.out.println("here");
 			
+			int eqp_id= Integer.parseInt(request.getParameter("e_id"));
+			System.out.println("eqpid:"+eqp_id);
+				
+			request.setAttribute("equip_id", eqp_id);
+			request.setAttribute("status", 2);
+			
+		s.executeUpdate("update kar set age=19 where no="+eqp_id);	
 			System.out.println("updated");
-			disp();
+				
+			System.out.println(eqp_id);
+			
+			s.executeUpdate("delete from down_table where equip_id="+eqp_id);
+						
+			ServletContext sc = request.getServletContext();
+			
+	RequestDispatcher rd =sc.getRequestDispatcher("/temp");
+					rd.forward(request, response);
 		}
+		
 		catch(Exception e)
 		{
 			e.printStackTrace();
-		}
+		}	
 	}
 	
 	
-	void disp()
-	{
-		Connection con=null;
-		
-	try
-	{
-		Class.forName("oracle.jdbc.driver.OracleDriver");
-		con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","scott","tiger");
 	
-		Statement s=con.createStatement();
-		
-	
-		ResultSet rs=s.executeQuery("select * from kar");//since the query returns many rows.. so we use result set
-		System.out.println("no 	name 	age");
-		while(rs.next())
-		{
-		
-			System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getInt(3));//must give the proper order for the corresponding col name...1 2 3 are the index positions of the cols in table
-			
-		}
-	}
-	catch(Exception e)
-	{
-		e.printStackTrace();
-	}
-	}
 
 }
