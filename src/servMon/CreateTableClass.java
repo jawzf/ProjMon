ad85e85dd62d4fad9c2b6ba7556fa6443c1b1b8d
Awesome Account <Awesome Account@java3-15PC>
2015-08-21 10:00:47 +0530

    Table class updated

--- src/servMon/CreateTableClass.java
+++ src/servMon/CreateTableClass.java
@@ -13,12 +13,11 @@ public class CreateTableClass {
 			Statement stmt; 
 		con=DriverManager.getConnection( "jdbc:oracle:thin:@localhost:1521:orcl","scott","tiger"); 
 		stmt=con.createStatement();  
-		String query="create table statusTable (equipID varchar(20) NOT NULL UNIQUE,custID varchar(20) NOT NULL,address varchar(30),email varchar(30),status varchar(10) check( status in ('UP','DOWN','IN-PROGRESS')))";
-		stmt.executeUpdate(query);
 		
-		query="create table downTable (equipID varchar(20) NOT NULL ,custID varchar(20) NOT NULL,address varchar(30) NOT NULL,email varchar(30) NOT NULL,status varchar(10) check( status in ('DOWN','IN-PROGRESS')))";
+		String query="create table statusTable (equipID varchar(20) NOT NULL UNIQUE,custID varchar(20) NOT NULL,address varchar(30) NOT NULL,email varchar(30),status varchar(10) check( status in ('UP','DOWN','IN-PROGRESS')))";
 		stmt.executeUpdate(query);
-
+		System.out.println("sTa");
+		
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -29,8 +28,9 @@ public class CreateTableClass {
 			Statement stmt; 
 		con=DriverManager.getConnection( "jdbc:oracle:thin:@localhost:1521:orcl","scott","tiger"); 
 		stmt=con.createStatement();  
-		String query="create table downTable (equipID number NOT NULL ,custID number NOT NULL,status varchar(10) check( status in ('DOWN','IN-PROGRESS')))";
+		String query="create table downTable (equipID varchar(20) NOT NULL ,custID varchar(20) NOT NULL,address varchar(30) NOT NULL,email varchar(30) NOT NULL,status varchar(10) check( status in ('DOWN','IN-PROGRESS')))";
 		stmt.executeUpdate(query);
+		System.out.println("dTa");
 
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
