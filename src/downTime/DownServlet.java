13029458bccaa997f9a76a1cf8458c5096d5460d
Joseph Kuriackal <jokuriackal@gmail.com>
2015-08-22 21:58:43 +0530

    Minor Updates

--- src/downTime/DownServlet.java
+++ src/downTime/DownServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/DownClass")
+@WebServlet("/DownServlet")
 public class DownServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
