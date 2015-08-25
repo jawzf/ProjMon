d576c5deda2b27548c7abcdd360c49e5d142432d
Awesome Account <Awesome Account@java3-15PC>
2015-08-25 11:13:44 +0530

    dbScript added

---
+++ dbScript.sql
@@ -0,0 +1,43 @@
+--------------------------------------------------------
+--  File created - Tuesday-August-25-2015   
+--------------------------------------------------------
+--------------------------------------------------------
+
+--------------------------------------------------------
+--  DDL for Table STATUSTABLE
+--------------------------------------------------------
+
+  CREATE TABLE "STATUSTABLE" 
+   (	"EQUIPID" VARCHAR2(20), 
+	"CUSTID" VARCHAR2(20), 
+	"ADDRESS" VARCHAR2(30), 
+	"EMAIL" VARCHAR2(30), 
+	"STATUS" VARCHAR2(10)
+   ) ;
+--------------------------------------------------------
+--  DDL for Table ASSIGNTABLE
+--------------------------------------------------------
+
+  CREATE TABLE "ASSIGNTABLE" 
+   (	"EQUIPID" VARCHAR2(20), 
+	"TECHNICIANID" VARCHAR2(20)
+   ) ;
+--------------------------------------------------------
+--  DDL for Table DOWNTABLE
+--------------------------------------------------------
+
+  CREATE TABLE "DOWNTABLE" 
+   (	"EQUIPID" VARCHAR2(20), 
+	"CUSTID" VARCHAR2(20), 
+	"ADDRESS" VARCHAR2(30), 
+	"EMAIL" VARCHAR2(30), 
+	"STATUS" VARCHAR2(10)
+   ) ;
+
+
+
+  CREATE UNIQUE INDEX "SYS_C0011253" ON "STATUSTABLE" ("EQUIPID") 
+  ;
+
+
+
