ad928babf894e17756ab3f5896be1c7677c91052
Awesome Account <Awesome Account@java3-15PC>
2015-08-27 17:30:29 +0530

    All Done for now!

---
+++ data.sql
@@ -0,0 +1,6 @@
+Insert into STATUSTABLE (EQUIPID,CUSTID,ADDRESS,EMAIL,STATUS) values ('987654','444444','Delhi','beingprax@gmail.com','UP');
+Insert into STATUSTABLE (EQUIPID,CUSTID,ADDRESS,EMAIL,STATUS) values ('C6D5','74hf74','Chennai','durejaishu@gmail.com','UP');
+Insert into STATUSTABLE (EQUIPID,CUSTID,ADDRESS,EMAIL,STATUS) values ('879gd65f','2122c3p0','Shimla','sushrutsood@gmail.com','UP');
+Insert into STATUSTABLE (EQUIPID,CUSTID,ADDRESS,EMAIL,STATUS) values ('hfjflkf8','nbvmjjff','Bangalore','karthika18j@gmail.com','UP');
+Insert into STATUSTABLE (EQUIPID,CUSTID,ADDRESS,EMAIL,STATUS) values ('123456','yoloyolo','New York','jokuriackal@gmail.com','UP');
+Insert into STATUSTABLE (EQUIPID,CUSTID,ADDRESS,EMAIL,STATUS) values ('4444','9797','6B,Block 3,Kences Brindavan, Kilpauk,Chennai','asd','UP');
