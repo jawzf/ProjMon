5693182c42cce4ac17051ef3b47f0b9ae6607153
Awesome Account <Awesome Account@java3-15PC>
2015-08-28 13:02:20 +0530

    credential updates

--- src/dbConnection/Provider.java
+++ src/dbConnection/Provider.java
@@ -3,7 +3,7 @@ package dbConnection;
 public interface Provider {
 	String DRIVER = "oracle.jdbc.driver.OracleDriver";
 	String CONNECTION_URL = "jdbc:oracle:thin:@localhost:1521:orcl";
-	String USERNAME = "scott";
-	String PASSWORD = "tiger";
+	String USERNAME = "monitoring";
+	String PASSWORD = "twentyfifteen";
 
 }
