5d0f3f8ad0626f1571467f94cb5dc33aee9e9893
Awesome Account <Awesome Account@java3-15PC>
2015-08-28 11:06:50 +0530

    Done

--- src/pullDataPackage/pullEmailClass.java
+++ src/pullDataPackage/pullEmailClass.java
@@ -2,24 +2,37 @@ package pullDataPackage;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Properties;
 
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
 
+import servMon.contRun;
+
 public class pullEmailClass {
 	
 	public String getEmail(String custid) throws IOException
 	{
+		
+		InputStream iStream=contRun.class.getClassLoader().getResourceAsStream("url.properties");
+		Properties props=new Properties();
+		props.load(iStream);
+		String yoorl=props.getProperty("url");
+		String port=props.getProperty("port");		
+		
+		
+		
 		System.out.println("called");
 		String e="";
 		int responseCode = 0;
 		String arr="";
-		String api = "http://192.168.1.27:8080/OrderManagement/rest/om/profilePull/"+custid;
+		String api = "http://"+yoorl+":"+port+"/OrderManagement/rest/om/profilePull/"+custid;
 				
 		URL url = new URL(api);
 		HttpURLConnection httpConnection = (HttpURLConnection) url.openConnection();
@@ -37,7 +50,7 @@ public class pullEmailClass {
 			jsonReader.close();
 			System.out.println(jsonObj.toString());
 			e=jsonObj.getJsonObject("customerdetails").getString("email");
-			System.out.println(e);
+			System.out.println("Email:"+e);
 			
 		}
 		return e;
---
+++ src/pullDataPackage/pullEmailClassTest.java
@@ -0,0 +1,35 @@
+package pullDataPackage;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class pullEmailClassTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetEmail()  {
+		pullEmailClass pec=new pullEmailClass();
+		try {
+			String res=pec.getEmail("32");
+			System.out.println(""+res);
+			assertNotNull(res);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
