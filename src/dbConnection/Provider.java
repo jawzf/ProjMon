1dc919f6b1612d7389dbb39c509cf7893639d96d
Administrator <Administrator@scsbwin-213231.ITTPA.EBIZ.VERIZON.COM>
2015-08-31 05:17:11 -0700

    XE update to jdbc connector

--- src/dbConnection/Provider.java
+++ src/dbConnection/Provider.java
@@ -2,7 +2,7 @@ package dbConnection;
 
 public interface Provider {
 	String DRIVER = "oracle.jdbc.driver.OracleDriver";
-	String CONNECTION_URL = "jdbc:oracle:thin:@localhost:1521:orcl";
+	String CONNECTION_URL = "jdbc:oracle:thin:@localhost:1521:xe";
 	String USERNAME = "monitoring";
 	String PASSWORD = "twentyfifteen";
 
