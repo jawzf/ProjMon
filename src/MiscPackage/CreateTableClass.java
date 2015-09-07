07281b30de5c0764f58599d2f78a58e4bec14dcb
Joseph Kuriackal <jokuriackal@gmail.com>
2015-09-07 22:29:04 +0530

    dummy values updated

--- src/MiscPackage/CreateTableClass.java
+++ src/MiscPackage/CreateTableClass.java
@@ -20,17 +20,17 @@ public class CreateTableClass {
 		con=ConnectionProvider.getCon();
 		stmt=con.createStatement();  
 		
-		String query="Insert into STATUSTABLE (EQUIPID,CUSTID,ADDRESS,EMAIL,STATUS) values ('987654','444444','Delhi','beingprax@gmail.com','UP')";
+		String query="Insert into STATUSTABLE (EQUIPID,CUSTID,ADDRESS,EMAIL,STATUS) values ('987654',444444,'Delhi','beingprax@gmail.com','UP')";
 		stmt.executeUpdate(query);
-		query="Insert into STATUSTABLE (EQUIPID,CUSTID,ADDRESS,EMAIL,STATUS) values ('C6D5','74hf74','Chennai','durejaishu@gmail.com','UP')";
+		query="Insert into STATUSTABLE (EQUIPID,CUSTID,ADDRESS,EMAIL,STATUS) values ('C6D5',744474,'Chennai','durejaishu@gmail.com','UP')";
 		stmt.executeUpdate(query);
-		query="Insert into STATUSTABLE (EQUIPID,CUSTID,ADDRESS,EMAIL,STATUS) values ('879gd65f','2122c3p0','Shimla','sushrutsood@gmail.com','UP')";
+		query="Insert into STATUSTABLE (EQUIPID,CUSTID,ADDRESS,EMAIL,STATUS) values ('879gd65f',2122000,'Shimla','sushrutsood@gmail.com','UP')";
 		stmt.executeUpdate(query);
-		query="Insert into STATUSTABLE (EQUIPID,CUSTID,ADDRESS,EMAIL,STATUS) values ('hfjflkf8','nbvmjjff','Bangalore','karthika18j@gmail.com','UP')";
+		query="Insert into STATUSTABLE (EQUIPID,CUSTID,ADDRESS,EMAIL,STATUS) values ('hfjflkf8',747474,'Bangalore','karthika18j@gmail.com','UP')";
 		stmt.executeUpdate(query);
-		query="Insert into STATUSTABLE (EQUIPID,CUSTID,ADDRESS,EMAIL,STATUS) values ('123456','yoloyolo','New York','jokuriackal@gmail.com','UP')";
+		query="Insert into STATUSTABLE (EQUIPID,CUSTID,ADDRESS,EMAIL,STATUS) values ('123456',888888,'New York','jokuriackal@gmail.com','UP')";
 		stmt.executeUpdate(query);
-		query="Insert into STATUSTABLE (EQUIPID,CUSTID,ADDRESS,EMAIL,STATUS) values ('4444','9797','6B,Block 3,Kences Brindavan, Kilpauk,Chennai','asd','UP')";
+		query="Insert into STATUSTABLE (EQUIPID,CUSTID,ADDRESS,EMAIL,STATUS) values ('4444',4565655,'6B,Block 3,Kences Brindavan, Kilpauk,Chennai','asd','UP')";
 		stmt.executeUpdate(query);
 		}
 		catch(Exception e)
